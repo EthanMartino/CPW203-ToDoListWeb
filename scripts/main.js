d4dcc41064f4368416604ade4cabbf24dd21dfab
Ethan Martino <49508204+ET7026@users.noreply.github.com>
2019-10-22 14:47:12 -0700

    Adding and displaying a todo item is functional

--- scripts/main.js
+++ scripts/main.js
@@ -7,4 +7,48 @@ var ToDoItem = /** @class */ (function () {
     }
     return ToDoItem;
 }());
-var myItem = new ToDoItem("DO THE FUCKING THING");
+var myItem = new ToDoItem("Finish Class");
+window.onload = function () {
+    var addBtn = document.querySelector("form > input[type=button]");
+    addBtn.onclick = main;
+};
+function main() {
+    var item = getItem();
+    displayToDoItem(item);
+    //Save todo
+}
+/**
+ * Creates and returns ToDoItem object based on user input in the form
+ */
+function getItem() {
+    var title = document.getElementById("title").value;
+    var deadline = document.getElementById("deadline").value;
+    var item = new ToDoItem(title);
+    item.deadline = new Date(deadline);
+    item.isCompleted = false;
+    return item;
+}
+/**
+ * Displays the given ToDoItem object on the webpage
+ * @param item The ToDoList oject to be displayed
+ */
+function displayToDoItem(item) {
+    //Create div to put the item in
+    var div = document.createElement("div");
+    //Moves clicked div into "Completed" div
+    div.onclick = markAsComplete;
+    //Adds an <input type="checkbox"> with the title of the item into the created div
+    div.innerHTML = '<input type="checkbox">' + item.title;
+    //Put new div into the ToDo div on the webpage
+    var displayDiv = document.getElementById("todo");
+    displayDiv.appendChild(div);
+    console.log(div);
+}
+/**
+ * Move selected item to the "Completed" div on webpage
+ */
+function markAsComplete() {
+    var currItem = this;
+    var completedItems = document.getElementById("completed");
+    completedItems.appendChild(currItem);
+}
--- typescripts/main.ts
+++ typescripts/main.ts
@@ -11,4 +11,61 @@ class ToDoItem{
     }
 }
 
-let myItem = new ToDoItem("DO THE FUCKING THING"); 
+let myItem = new ToDoItem("Finish Class"); 
+
+window.onload = function(){
+    let addBtn:HTMLInputElement = document.querySelector("form > input[type=button]");
+    addBtn.onclick = main;
+}
+
+function main():void{
+    let item:ToDoItem = getItem();
+
+    displayToDoItem(item);
+
+    //Save todo
+}
+
+/**
+ * Creates and returns ToDoItem object based on user input in the form
+ */
+function getItem():ToDoItem{
+    let title:string = (<HTMLInputElement>document.getElementById("title")).value;
+    let deadline:string = (<HTMLInputElement>document.getElementById("deadline")).value;
+
+    let item = new ToDoItem(title);
+    item.deadline = new Date(deadline);
+    item.isCompleted = false;
+
+    return item;
+}
+
+/**
+ * Displays the given ToDoItem object on the webpage
+ * @param item The ToDoList oject to be displayed
+ */
+function displayToDoItem(item:ToDoItem):void{
+    //Create div to put the item in
+    let div = document.createElement("div");
+
+    //Moves clicked div into "Completed" div
+    div.onclick = markAsComplete;
+
+    //Adds an <input type="checkbox"> with the title of the item into the created div
+    div.innerHTML = '<input type="checkbox">' + item.title;
+
+    //Put new div into the ToDo div on the webpage
+    let displayDiv = <HTMLDivElement>document.getElementById("todo");
+    displayDiv.appendChild(div);
+
+    console.log(div);
+}
+
+/**
+ * Move selected item to the "Completed" div on webpage
+ */
+function markAsComplete():void{
+    let currItem = <HTMLDivElement>this;
+    let completedItems = document.getElementById("completed");
+    completedItems.appendChild(currItem);
+}
